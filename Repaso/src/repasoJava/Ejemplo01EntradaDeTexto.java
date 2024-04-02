640237c5fc6dc9a25f71a908a9dbb8fab13d92af
Luciamarinm <luciamarinmartinez94@gmail.com>
2024-04-02 17:38:46 +0100

    Clase JOptionPane

--- Repaso/src/repasoJava/Ejemplo01EntradaDeTexto.java
+++ Repaso/src/repasoJava/Ejemplo01EntradaDeTexto.java
@@ -2,6 +2,8 @@ package repasoJava;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Ejemplo01EntradaDeTexto {
 
 	public static void main(String[] args) {
@@ -10,7 +12,11 @@ public class Ejemplo01EntradaDeTexto {
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Introduce tu edad");
 		edad = entrada.nextInt(); //Devuelve un Int
-		System.out.println("Tu edad es: " + edad);
+		System.out.println("Introduce tu nombre");
+		entrada.nextLine(); //Si no lo ponemos, el enter se perderá y el programa lo omitirá
+		String nombre = entrada.nextLine(); //Si no lo usamos al inicio no guardará el nombre 
+		
+		System.out.println("Tu edad es: " + edad + " y tu nombre es " + nombre);
 		System.out.println("¿Cual es la edad de tu hermano?");
 		int edadHermano = entrada.nextInt();
 		if(edad < edadHermano) { //Para concatenar e interiormente hacer sumas, hay que usar parentesis
@@ -20,6 +26,7 @@ public class Ejemplo01EntradaDeTexto {
 		} else {
 			System.out.println("Teneis la misma edad");
 		}
+
 		
 		entrada.close(); //cerrar clase Scanner
 	}
---
+++ Repaso/src/repasoJava/Ejemplo04ClaseJOptionPane.java
@@ -0,0 +1,30 @@
+package repasoJava;
+
+import javax.swing.JOptionPane;
+
+public class Ejemplo04ClaseJOptionPane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Esto abrirá una ventana de dialogo y entrada de texto
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre por favor:"); //Tipo String
+		System.out.println("Tu nombre es: " + nombre);
+		//Para pedir otros tipos de datos
+		int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad por favor:"));	
+		System.out.println("Tu edad es: " + edad);
+		
+		//Ejercicio: 
+		/*Al ejecutar el programa nos debe pedir introducir dos valores numéricos utilizando una ventana 
+		de tipo showInputDialog:
+		Una base
+		Un exponente
+		Una vez introducidos los dos valores, 
+		el programa mostrará en consola el resultado de elevar la base al exponente*/
+		int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número por favor:"));
+		int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número por favor:"));
+		double potencia = Math.pow(numero1, numero2);
+		System.out.println("El resultado de " + numero1 + " elevado a " + 3 + " es " + potencia);
+	}
+
+}
